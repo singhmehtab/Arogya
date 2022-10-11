2c3220b9791a236b6abc8bf9b360697f0aca6a7a
Mehtab Singh <singhmehtab571@gmail.com>
2022-10-10 20:47:55 -0400

    Initial Commit

---
+++ src/main/java/com/spm/arogya/ArogyaApplication.java
@@ -0,0 +1,13 @@
+package com.spm.arogya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArogyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArogyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spm/arogya/ArogyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spm.arogya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArogyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
